92c72f2de1534985d49ba88d86310777c4157129
Ancient <gamesworldcraft@gmail.com>
2026-03-24 16:34:15 +0800

    fix: change error code

--- crates/error-code/src/auth.rs
+++ crates/error-code/src/auth.rs
@@ -1,8 +1,8 @@
-// Auth route business error codes: -1000..-1039
-pub const USERNAME_ALREADY_EXISTS: i16 = -1000;
-pub const EMAIL_ALREADY_EXISTS: i16 = -1001;
-pub const USER_NOT_FOUND: i16 = -1002;
-pub const USER_DISABLED: i16 = -1003;
-pub const PASSWORD_HASH_FAILED: i16 = -1004;
-pub const PASSWORD_HASH_PARSE_FAILED: i16 = -1005;
-pub const PASSWORD_INVALID: i16 = -1006;
+// Auth route business error codes: -10000..-10009
+pub const USERNAME_ALREADY_EXISTS: i16 = -10000;
+pub const EMAIL_ALREADY_EXISTS: i16 = -10001;
+pub const USER_NOT_FOUND: i16 = -10002;
+pub const USER_DISABLED: i16 = -10003;
+pub const PASSWORD_HASH_FAILED: i16 = -10004;
+pub const PASSWORD_HASH_PARSE_FAILED: i16 = -10005;
+pub const PASSWORD_INVALID: i16 = -10006;
